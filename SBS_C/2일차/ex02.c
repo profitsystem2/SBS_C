f35048b3e553ab806228c583153db95d21e89de9
skyblue1232 <skybluekmg@gmail.com>
2022-02-09 15:51:42 +0900

    ddd

---
+++ SBS_C/2일차/ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    short sh = 32767;
+    int in= 2147483647;
+    long ln= 2147483647;
+    long long lln= 123451234512345;
+
+    printf("sh : %d\n",sh);
+    printf("in : %d\n",in);
+    printf("ln : %ld\n",ln);
+    printf("lln : %lld\n",lln);
+    return 0;
+}
